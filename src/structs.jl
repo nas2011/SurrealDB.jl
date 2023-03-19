d4fbb354853142bc2d7c2c7519978e20461328b3
nas2011 <nick@nicksexson.com>
2023-03-19 17:22:07 -0400

    update structs

--- src/structs.jl
+++ src/structs.jl
@@ -10,3 +10,36 @@ mutable struct SurrealDBConnection
     SurrealDBConnection(user,password;database="",namespace="") = new("http://localhost:8000",user,password,database,namespace)
 end
 
+
+struct SurrealNS
+    ns::String
+end
+
+struct SurrealDB
+    ns::SurrealNS
+    name::String
+end
+
+mutable struct SurrealField
+    name::String
+    type::String
+    assertion::String
+    SurrealField(name::String,type::String,assertion::String) = new(name,type,assertion)
+    SurrealField(name::String,type::String) = new(name,type,"")
+end
+
+
+
+mutable struct SurrealTable
+    ns::SurrealNS
+    db::SurrealDB
+    name::String
+    schema::String
+    fields::Vector{SurrealField}
+
+    SurrealTable(ns,db,name,schema) = 
+    !in(lowercase(schema),["schemaful","schemaless"]) ? error("must be schemaful or schemaless") : new(ns,db,name,schema,[])
+end
+
+
+
