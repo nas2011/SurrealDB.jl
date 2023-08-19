01c6632f389baa445bd824592674bd4d8014e521
nas2011 <nick@nicksexson.com>
2023-08-19 15:56:11 -0400

    update to ws

--- src/core.jl
+++ src/core.jl
@@ -1,19 +1,104 @@
-using HTTP, JSON3, DataFrames, Base64
-include("structs.jl")
-
-
-function execute(conn::SurrealDBConnection,query::String)
-    ep = string(conn.url,"/sql")
-    header = [
-        "Authorization" => string("Basic ", base64encode("$(conn.user):$(conn.password)")),
-        "Accept" => "application/json",
-        "NS" => conn.namespace,
-        "DB" => conn.database,
-    ]
-    HTTP.request("POST",ep,headers = header, body=query) |>
-    y-> JSON3.read(y.body|>String)
+begin
+    using HTTP, JSON3, DataFrames, Base64, UUIDs, Random
+    using HTTP.WebSockets: send, receive
+    include("structs.jl")
 end
 
+
+
+wsc = SurrealConnection("ws://localhost:8000/rpc","root","root",db = "test", ns = "test")
+htc = SurrealConnection("http://localhost:8000","root","root", db = "test", ns = "test")
+
+
+function signin(conn::SurrealConnection;root::Bool=true,sc::Union{Nothing,String}=nothing)
+    additional = ""
+    if !root
+        scope = isnothing(sc) ? "" : "\"SC\": \"$sc\""
+        ns = "\"NS\" : \"$(conn.ns)\","
+        db = "\"DB\" : \"$(conn.db)\","
+        additional = ns * db * scope
+    end    
+    id = conn.ws.id |> string
+    body = """{
+        "id": "$id",
+        "method": "signin",
+        "params": [
+            {
+                "user": "$(conn.user)",
+                "pass": "$(conn.pass)",
+                $additional
+            }
+        ]
+    }"""
+
+    send(conn.ws,body)
+    receive(conn.ws)
+end
+   
+signin(wsc)
+
+function use(conn::SurrealConnection)
+    id = conn.ws.id |> string
+    use = """{
+        "id": "$id",
+        "method": "use",
+        "params": [ "$(conn.ns)", "$(conn.db)" ]
+    }"""
+    send(conn.ws,use)
+    receive(conn.ws)
+end
+
+use(wsc)
+
+function select(conn::SurrealConnection,param::String)
+    id = conn.ws.id |> string
+    body = """{
+        "id": "$id",
+        "method": "select",
+        "params": [
+           "$param"
+        ]
+    }"""
+    send(conn.ws,body)
+    receive(conn.ws)
+end
+
+select(wsc,"person") |> JSON3.pretty
+
+function varstring(vars::Dict{String,String})
+    join(["\"$k\" : \"$v\"" for (k,v) in pairs(vars)], ",\n")
+end
+
+function query(conn::SurrealWSConnection,query::String,vars::Dict{String,String}=Dict{String,String}())
+    id = conn.ws.id |> string
+    varstr = varstring(vars)
+    body = """{
+        "id": "$id",
+        "method": "query",
+        "params": [
+            "$query",
+            {
+                $varstr
+            }
+        ]
+    }"""
+    send(conn.ws,body)
+    receive(conn.ws)
+end
+
+
+function execute(conn::SurrealConnection,query::String)
+    ep = string(conn.url,"/sql")
+    header = [
+        "Authorization" => string("Basic ", base64encode("$(conn.user):$(conn.pass)")),
+        "Accept" => "application/json",
+        "NS" => conn.ns,
+        "DB" => conn.db,
+    ]
+    HTTP.request("POST",ep,headers = header, body=query).body |> String |> JSON3.read
+end
+
+df = execute(htc,q) |> todf
 
 function todf(result::JSON3.Array)
     if result[1].result == []
--- src/structs.jl
+++ src/structs.jl
@@ -1,45 +1,57 @@
-mutable struct SurrealDBConnection
+function rawws(url,headers =[])
+    headers = [
+        "Upgrade" => "websocket",
+        "Connection" => "Upgrade",
+        "Sec-WebSocket-Key" => base64encode(rand(Random.RandomDevice(), UInt8, 16)),
+        "Sec-WebSocket-Version" => "13",
+        headers...
+    ]
+    r = HTTP.openraw("GET",url,headers)[1]
+
+    ws = WebSockets.WebSocket(r)
+    return ws
+end
+
+@kwdef mutable struct SurrealConnection
+    url::String = "ws://localhost:8000/rpc"
+    user::String = "root"
+    pass::String = "root"
+    ns::String = ""
+    db::String = ""
+    type::String
+    connected::Bool = false
+    ws::Union{HTTP.WebSockets.WebSocket,Nothing}
+    SurrealConnection(url, user, pass; ns="", db="") = (
+        x = new(url, user, pass, ns, db);
+        x.ws = occursin("ws:", x.url) ? rawws(x.url) : nothing;
+        x.type = occursin("ws:", x.url) ? "websocket" : "HTTP";
+        x.connected = x.type == "websocket" ? HTTP.isopen(x.ws.io) : false;
+        return x
+    )
+end
+
+
+mutable struct SurrealWSConnection
     url::String
     user::String
-    password::String
-    database::String
-    namespace::String
-    SurrealDBConnection(url,user,password) = new(url,user,password,"","")
-    SurrealDBConnection(user,password) = new("http://localhost:8000",user,password,"","")
-    SurrealDBConnection(user,password) = new("http://localhost:8000",user,password,"","")
-    SurrealDBConnection(user,password;database="",namespace="") = new("http://localhost:8000",user,password,database,namespace)
-end
-
-
-struct SurrealNS
+    pass::String
+    db::String
     ns::String
-end
-
-struct SurrealDB
-    ns::SurrealNS
-    name::String
-end
-
-mutable struct SurrealField
-    name::String
-    type::String
-    assertion::String
-    SurrealField(name::String,type::String,assertion::String) = new(name,type,assertion)
-    SurrealField(name::String,type::String) = new(name,type,"")
+    connected::Bool
+    ws::HTTP.WebSockets.WebSocket
+    SurrealWSConnection(url,user,pass) = new(url,user,pass,"","",true,rawws(url))
+    SurrealWSConnection(user,pass) = new("ws://localhost:8000/rpc",user,pass,"","",true,rawws(url))
+    SurrealWSConnection(user,pass;db="",ns="") = new("ws://localhost:8000/rpc",user,pass,db,ns,true,rawws("ws://localhost:8000/rpc"))
 end
 
 
-
-mutable struct SurrealTable
-    ns::SurrealNS
-    db::SurrealDB
-    name::String
-    schema::String
-    fields::Vector{SurrealField}
-
-    SurrealTable(ns,db,name,schema) = 
-    !in(lowercase(schema),["schemaful","schemaless"]) ? error("must be schemaful or schemaless") : new(ns,db,name,schema,[])
+mutable struct SurrealHTTPConnection
+    url::String
+    user::String
+    pass::String
+    db::String
+    ns::String
+    SurrealHTTPConnection(user,pass) = new("http://localhost:8000",user,pass,"","")
+    SurrealHTTPConnection(url,user,pass) = new(url,user,pass,"","")
+    SurrealHTTPConnection(user,pass;db="",ns="") = new("http://localhost:8000",user,pass,db,ns)
 end
-
-
-
